3ab983b79cd5b37eb17d7a9da09c83c467f6fde3
Adamyr159 <adamyr159@Gmail.com>
2025-05-07 11:28:44 +0700

    praktikum 9 - Koneksi Database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/produk', function () {
+    return view('produk.index');
+});
+
+Route::get('/produk/{id}', function ($id) {
+    return view('produk.index', ['id' => $id]);
+});
+
+
+Route::get('/books', [BookController::class, 'index']);
